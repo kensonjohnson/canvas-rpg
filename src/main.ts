1e1f8eef6af982350d02fa48cc14db7d3d7339f0
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-01 16:29:22 -0600

    Refactor to use new Sprite class

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,49 @@
 import "./style.css";
 import { resources } from "./Resource";
+import { Sprite } from "./Sprite";
+import { Vector2 } from "./Vector2";
 
 const canvas = document.querySelector("#game-canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-function draw() {
-  const sky = resources.images.sky;
-  if (sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0);
-  }
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+});
 
-  const ground = resources.images.ground;
-  if (ground.isLoaded) {
-    ctx.drawImage(ground.image, 0, 0);
-  }
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+});
+
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hframes: 3,
+  vframes: 8,
+  frame: 1,
+});
+
+const shadow = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+});
+
+const heroPosition = new Vector2(16 * 6, 16 * 5);
+const heroOffset = new Vector2(-8, -21);
+
+function draw() {
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+
+  const heroPositionX = heroPosition.x + heroOffset.x;
+  const heroPositionY = heroPosition.y + heroOffset.y;
+
+  shadow.drawImage(ctx, heroPositionX, heroPositionY);
+  hero.drawImage(ctx, heroPositionX, heroPositionY);
 }
 
-setInterval(draw, 300);
+setInterval(() => {
+  hero.frame += 1;
+  draw();
+}, 300);
