6e8cacdbd846aaaa67bc00015f5243be27463efe
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-02 19:32:16 -0600

    Add exit object

---
+++ src/objects/Exit/Exit.ts
@@ -0,0 +1,33 @@
+import { GameObject } from "@/GameObject";
+import { resources } from "@/Resource";
+import { Sprite } from "@/Sprite";
+import { Vector2 } from "@/Vector2";
+import { events } from "@/Events";
+
+export class Exit extends GameObject {
+  constructor(x: number, y: number) {
+    super({
+      position: new Vector2(x, y),
+    });
+
+    this.addChild(
+      new Sprite({
+        resource: resources.images.exit,
+      })
+    );
+  }
+
+  ready() {
+    events.on("HERO_POSITION", this, (position: Vector2) => {
+      const roundedHeroPositionX = Math.round(position.x);
+      const roundedHeroPositionY = Math.round(position.y);
+
+      if (
+        roundedHeroPositionX === this.position.x &&
+        roundedHeroPositionY === this.position.y
+      ) {
+        events.emit("HERO_EXITS");
+      }
+    });
+  }
+}
