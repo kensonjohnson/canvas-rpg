08df02b0d00f05df1037a9adbc53632cb8b069e8
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-02 21:18:30 -0600

    Prepare class for storing hero starting position

--- src/objects/Level/Level.ts
+++ src/objects/Level/Level.ts
@@ -1,11 +1,17 @@
 import { GameObject, GameObjectConfig } from "@/GameObject";
 import { Sprite } from "@/Sprite";
+import { Vector2 } from "@/Vector2";
+
+export type LevelConfig = GameObjectConfig & {
+  heroPosition?: Vector2;
+};
 
 export class Level extends GameObject {
+  heroPosition?: Vector2;
   background?: Sprite;
   walls: Set<string>;
 
-  constructor(config: GameObjectConfig = {}) {
+  constructor(config: LevelConfig = {}) {
     super(config);
     this.background = undefined;
     this.walls = new Set<string>();
