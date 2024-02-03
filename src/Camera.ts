f9e09575e55e301d9a215c54ac26c29706cc25f8
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-02 21:19:19 -0600

    Update camera to be on hero at frame one

--- src/Camera.ts
+++ src/Camera.ts
@@ -6,16 +6,24 @@ export class Camera extends GameObject {
   constructor() {
     super({});
     events.on("HERO_POSITION", this, (position: Vector2) => {
-      const personHalf = 8;
-      const canvasWidth = 320;
-      const canvasHeight = 180;
-      const halfWidth = -personHalf + canvasWidth / 2;
-      const halfHeight = -personHalf + canvasHeight / 2;
-
-      this.position = new Vector2(
-        -position.x + halfWidth,
-        -position.y + halfHeight
-      );
+      this.centerPositionOnTarget(position);
     });
+
+    events.on("CHANGE_LEVEL", this, (newMap: { heroPosition: Vector2 }) => {
+      this.centerPositionOnTarget(newMap.heroPosition);
+    });
+  }
+
+  centerPositionOnTarget(position: Vector2) {
+    const personHalf = 8;
+    const canvasWidth = 320;
+    const canvasHeight = 180;
+    const halfWidth = -personHalf + canvasWidth / 2;
+    const halfHeight = -personHalf + canvasHeight / 2;
+
+    this.position = new Vector2(
+      -position.x + halfWidth,
+      -position.y + halfHeight
+    );
   }
 }
