5dd1acc71dedd59c31bf0892441060f94032138c
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-04 13:33:43 -0600

    Added walls around entire map

--- src/levels/OutdoorLevel1.ts
+++ src/levels/OutdoorLevel1.ts
@@ -35,17 +35,60 @@ export class OutdoorLevel1 extends Level {
     const rod = new Rod(gridCells(7), gridCells(6));
     this.addChild(rod);
 
-    this.walls.add("64,48"); // Tree
+    this.walls.add(`${gridCells(3)},${gridCells(2)}`); // Sky
+    this.walls.add(`${gridCells(4)},${gridCells(2)}`);
+    this.walls.add(`${gridCells(5)},${gridCells(2)}`);
+    this.walls.add(`${gridCells(6)},${gridCells(2)}`);
+    this.walls.add(`${gridCells(7)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(8)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(9)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(10)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(11)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(12)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(13)},${gridCells(1)}`);
+    this.walls.add(`${gridCells(15)},${gridCells(2)}`);
+    this.walls.add(`${gridCells(16)},${gridCells(3)}`);
+    this.walls.add(`${gridCells(16)},${gridCells(4)}`);
+    this.walls.add(`${gridCells(16)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(16)},${gridCells(6)}`);
+    this.walls.add(`${gridCells(15)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(14)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(13)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(12)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(11)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(10)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(9)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(8)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(7)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(6)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(5)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(4)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(3)},${gridCells(7)}`);
+    this.walls.add(`${gridCells(2)},${gridCells(6)}`);
+    this.walls.add(`${gridCells(2)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(2)},${gridCells(4)}`);
+    this.walls.add(`${gridCells(2)},${gridCells(3)}`);
+    this.walls.add(`${gridCells(2)},${gridCells(2)}`);
 
-    this.walls.add("64,64"); // squares
-    this.walls.add("64,80");
-    this.walls.add("80,64");
-    this.walls.add("80,80");
+    this.walls.add(`${gridCells(4)},${gridCells(3)}`); // Trees
+    this.walls.add(`${gridCells(14)},${gridCells(2)}`);
+    this.walls.add(`${gridCells(13)},${gridCells(4)}`);
 
-    this.walls.add("112,80"); // water
-    this.walls.add("128,80");
-    this.walls.add("144,80");
-    this.walls.add("160,80");
+    this.walls.add(`${gridCells(4)},${gridCells(4)}`); // squares
+    this.walls.add(`${gridCells(4)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(5)},${gridCells(4)}`);
+    this.walls.add(`${gridCells(5)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(8)},${gridCells(3)}`);
+    this.walls.add(`${gridCells(9)},${gridCells(3)}`);
+
+    this.walls.add(`${gridCells(7)},${gridCells(5)}`); // water
+    this.walls.add(`${gridCells(8)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(9)},${gridCells(5)}`);
+    this.walls.add(`${gridCells(10)},${gridCells(5)}`);
+
+    this.walls.add(`${gridCells(12)},${gridCells(6)}`); // rocks
+    this.walls.add(`${gridCells(13)},${gridCells(6)}`);
+    this.walls.add(`${gridCells(14)},${gridCells(6)}`);
   }
 
   ready(): void {
